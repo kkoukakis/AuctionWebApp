b4b3305637cc5cba07a34249c02377e0cbbfbe2e
kkoukakis@github.com <kkoukakis@windowslive.com>
2019-09-27 18:32:10 +0300

    user_exists changes

--- Backend/api/auth/user_exists.js
+++ Backend/api/auth/user_exists.js
@@ -2,7 +2,7 @@ module.exports =  function user_exists (userid){
 var query = 'SELECT * from user WHERE UserID = \''+userid+'\'';
     //console.log(query);
     global.connection.query(query, function (error, results, fields) {
-        if (error) {console.log('AAAAAAAAAAAAAAAAAAAA') ; throw error;}
+        if (error) { throw error;}
        // console.log(results.length);
         if(results.length !== 0){
             //USER EXISTS
